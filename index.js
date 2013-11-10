373b0e2df85d84cf9a477bc56ac5d524a26b3d83
Ruben Vermeersch <ruben@rocketeer.be>
2013-11-10 17:09:14 +0100

    Only attempt to replace the body if there is one.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function () {
         res.end = function (body, encoding) {
             var header = res.getHeader('content-type');
 
-            if (header && header.match(/text\/html/)) {
+            if (header && header.match(/text\/html/) && body) {
                 body = body.replace(/<html(.*)manifest=(('|").*?\3)(.*)>/, "<html$1$4>");
                 if (res.data !== undefined && !res._header) {
                     res.setHeader('content-length', Buffer.byteLength(body, encoding));
