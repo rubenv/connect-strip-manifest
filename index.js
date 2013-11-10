83cbe19ce8fbce257283b93411eee2a64c3367b9
Ruben Vermeersch <ruben@rocketeer.be>
2013-11-10 15:52:20 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function () {
+    return function (req, res, next) {
+        var end = res.end;
+
+        res.end = function (body, encoding) {
+            var header = res.getHeader('content-type');
+
+            if (header && header.match(/text\/html/)) {
+                body = body.replace(/<html(.*)manifest=(('|").*?\3)(.*)>/, "<html$1$4>");
+                if (res.data !== undefined && !res._header) {
+                    res.setHeader('content-length', Buffer.byteLength(body, encoding));
+                }
+            }
+            end.call(res, body, encoding);
+        };
+        next();
+    };
+};
