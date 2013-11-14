8b95dd01feb1efd8046f9ff701617f0cbf5d9200
Ruben Vermeersch <ruben@rocketeer.be>
2013-11-14 16:33:16 +0100

    Protect against missing headers.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var rewrite = require('connect-body-rewrite');
 module.exports = function () {
     return rewrite({
         accept: function (res) {
-            return res.getHeader('content-type').match(/text\/html/);
+            return (res.getHeader('content-type') || '').match(/text\/html/);
         },
         rewrite: function (body) {
             return body.replace(/<html(.*)manifest=(('|").*?\3)(.*)>/, "<html$1$4>");
