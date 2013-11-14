fdf07742fa009ea8aad5e15940ef4cd6248be408
Ruben Vermeersch <ruben@rocketeer.be>
2013-11-14 10:21:11 +0100

    More fool-proof request rewriting.

--- index.js
+++ index.js
@@ -2,19 +2,24 @@
 
 module.exports = function () {
     return function (req, res, next) {
-        var end = res.end;
+        var write = res.write;
 
-        res.end = function (body, encoding) {
+        res.write = function (chunk, encoding, callback) {
+            var length = res.getHeader('content-length');
             var header = res.getHeader('content-type');
 
-            if (header && header.match(/text\/html/) && body) {
-                body = body.replace(/<html(.*)manifest=(('|").*?\3)(.*)>/, "<html$1$4>");
-                if (res.data !== undefined && !res._header) {
-                    res.setHeader('content-length', Buffer.byteLength(body, encoding));
-                }
+            if (header && header.match(/text\/html/) && chunk) {
+                chunk = chunk.toString();
+                var origLength = chunk.length;
+                chunk = chunk.replace(/<html(.*)manifest=(('|").*?\3)(.*)>/, "<html$1$4>");
+                var newLength = chunk.length;
+
+                res.setHeader('content-length', length - (origLength - newLength));
             }
-            end.call(res, body, encoding);
+
+            write.call(res, chunk, encoding, callback);
         };
+
         next();
     };
 };
