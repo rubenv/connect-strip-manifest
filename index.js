4974de47aaf7af9ace856fb62652350290969558
Ruben Vermeersch <ruben@rocketeer.be>
2013-11-14 15:09:23 +0100

    Use connect-body-rewrite.

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 'use strict';
 
+var rewrite = require('connect-body-rewrite');
+
 module.exports = function () {
-    return function (req, res, next) {
-        var write = res.write;
-
-        res.write = function (chunk, encoding, callback) {
-            var length = res.getHeader('content-length');
-            var header = res.getHeader('content-type');
-
-            if (header && header.match(/text\/html/) && chunk) {
-                chunk = chunk.toString();
-                var origLength = chunk.length;
-                chunk = chunk.replace(/<html(.*)manifest=(('|").*?\3)(.*)>/, "<html$1$4>");
-                var newLength = chunk.length;
-
-                res.setHeader('content-length', length - (origLength - newLength));
-            }
-
-            write.call(res, chunk, encoding, callback);
-        };
-
-        next();
-    };
+    return rewrite({
+        accept: function (res) {
+            return res.getHeader('content-type').match(/text\/html/);
+        },
+        rewrite: function (body) {
+            return body.replace(/<html(.*)manifest=(('|").*?\3)(.*)>/, "<html$1$4>");
+        }
+    });
 };
